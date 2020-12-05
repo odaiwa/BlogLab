2c513212fa7400666d06c6ac895985c1332f108f
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2020-12-06 01:40:12 +0200

    More Code

--- BlogAPI/BlogLabModels/Account/ApplicationUserIdentity.cs
+++ BlogAPI/BlogLabModels/Account/ApplicationUserIdentity.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace BlogLabModels.Account
 {
-    class ApplicationUserIdentity
+    public class ApplicationUserIdentity
     {
+        public int ApplicationUserId { get; set; }
+
+        public string Username { get; set; }
+
+        public string NormlizedUsername { get; set; }
+
+        public string Email { get; set; }
+
+        public string NormlizedEmail { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string PasswordHash { get; set; }
+
+
     }
 }
--- BlogAPI/BlogLabModels/Account/ApplicationUserLogin.cs
+++ BlogAPI/BlogLabModels/Account/ApplicationUserLogin.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlogLabModels.Account
 {
-    class ApplicationUserLogin
+    public class ApplicationUserLogin
     {
+        [Required(ErrorMessage ="Username is required")]
+        [MinLength(5,ErrorMessage ="Must be at least 5-20 characters")]
+        [MaxLength(20, ErrorMessage = "Must be at least 5-20 characters")]
+
+
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(10, ErrorMessage = "Must be at least 10-50 characters")]
+        [MaxLength(50, ErrorMessage = "Must be at least 10-50 characters")]
+        public string Password { get; set; }
+
+
+
     }
 }
