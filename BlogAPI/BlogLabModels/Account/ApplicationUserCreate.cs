c04f63ef4e37082792291eb384f49e188689f44f
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2020-12-06 01:38:47 +0200

    Code

--- BlogAPI/BlogLabModels/Account/ApplicationUserCreate.cs
+++ BlogAPI/BlogLabModels/Account/ApplicationUserCreate.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlogLabModels.Account
 {
-    class ApplicationUserCreate
+    public class ApplicationUserCreate : ApplicationUserLogin
     {
+        [MinLength(10, ErrorMessage = "Must be at least 10-30 characters")]
+        [MaxLength(30, ErrorMessage = "Must be at least 10-30 characters")]
+        public string Fullname { get; set; }
+
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress( ErrorMessage = "InValid Email Format")]
+        [MaxLength(30, ErrorMessage = "Can be at must 30 characters")]
+        public string Email { get; set; }
     }
 }
