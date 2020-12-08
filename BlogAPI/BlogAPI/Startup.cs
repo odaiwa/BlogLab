bad319726a170e5ede619d365e26b21b0f758fc2
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2020-12-08 16:04:05 +0200

    CloudiniryAPI

--- BlogAPI/BlogAPI/Startup.cs
+++ BlogAPI/BlogAPI/Startup.cs
@@ -7,15 +7,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogLab.Models.Settings;
+using Microsoft.Extensions.Configuration;
 
 namespace BlogAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudiniryOptions"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ BlogAPI/BlogLab.Services/IPhotoService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace BlogLab.Services
+{
+    public interface IPhotoService
+    {
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId);
+
+    }
+}
